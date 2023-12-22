ccf2ac895c4d1c41ea993fd62765c5840c579ef5
Dolbonya <alexandrsoloby@gmail.com>
2023-12-22 10:57:38 +0300

    fix route

--- routes/route.go
+++ routes/route.go
@@ -3,11 +3,6 @@ package routes
 import (
 	"context"
 	"currency-conversion/services"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -47,44 +42,44 @@ func Routes(addr *string) {
 
 	route.V1Routes()
 
-	router.Get("/", func(c *fiber.Ctx) error {
-		url := os.Getenv("url_all_currencies")
-		method := os.Getenv("method")
-		client := &http.Client{}
+	// router.Get("/", func(c *fiber.Ctx) error {
+	// 	url := os.Getenv("url_all_currencies")
+	// 	method := os.Getenv("method")
+	// 	client := &http.Client{}
 
-		req, err := http.NewRequest(method, url, nil)
-		if err != nil {
-			return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error when creating a request: %s", err))
-		}
+	// 	req, err := http.NewRequest(method, url, nil)
+	// 	if err != nil {
+	// 		return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error when creating a request: %s", err))
+	// 	}
 
-		apiKey := os.Getenv("API_KEY")
-		req.Header.Add("apikey", apiKey)
+	// 	apiKey := os.Getenv("API_KEY")
+	// 	req.Header.Add("apikey", apiKey)
 
-		res, err := client.Do(req)
-		if err != nil {
-			return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error when executing a request: %s", err))
-		}
-		defer res.Body.Close()
+	// 	res, err := client.Do(req)
+	// 	if err != nil {
+	// 		return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error when executing a request: %s", err))
+	// 	}
+	// 	defer res.Body.Close()
 
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error reading data from the response: %s", err))
-		}
+	// 	body, err := io.ReadAll(res.Body)
+	// 	if err != nil {
+	// 		return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error reading data from the response: %s", err))
+	// 	}
 
-		var result map[string]interface{}
-		if err := json.Unmarshal(body, &result); err != nil {
-			return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error during JSON parsing: %s", err))
-		}
+	// 	var result map[string]interface{}
+	// 	if err := json.Unmarshal(body, &result); err != nil {
+	// 		return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error during JSON parsing: %s", err))
+	// 	}
 
-		prettiedJSON, err := json.MarshalIndent(result, "", "  ")
-		if err != nil {
-			return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error when formatting JSON: %s", err))
-		}
+	// 	prettiedJSON, err := json.MarshalIndent(result, "", "  ")
+	// 	if err != nil {
+	// 		return c.Status(http.StatusInternalServerError).SendString(fmt.Sprintf("Error when formatting JSON: %s", err))
+	// 	}
 
-		return c.SendString(string(prettiedJSON))
-	})
+	// 	return c.SendString(string(prettiedJSON))
+	// })
 
-	if err := router.Listen(*addr); err != nil {
-		log.Fatal().Err(err).Msg("Can not start http server")
-	}
+	// if err := router.Listen(*addr); err != nil {
+	// 	log.Fatal().Err(err).Msg("Can not start http server")
+	// }
 }
